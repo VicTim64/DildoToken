540e62129fd6b185d9005031eeacfd061d4c734e
NeoTechAi <neotechfinancecity@gmail.com>
2025-07-14 08:07:48 +0300

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -2,84 +2,76 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative w-full min-h-[860px] overflow-hidden" style={{
-      background: "linear-gradient(to bottom, #003619, #013018)"
-    }}>
-      {/* Fundal pixelat */}
-      <div className="absolute top-[210px] left-1/2 -translate-x-1/2 w-full h-[540px] z-0">
+    <section className="relative w-full h-[900px] bg-gradient-to-b from-[#013018] to-[#014422] overflow-hidden">
+      {/* Background asset - center stripe */}
+      <div className="absolute top-[130px] left-1/2 transform -translate-x-1/2 w-[1300px] h-[550px] z-0">
         <Image
           src="/assets/asset_1.png"
-          alt="Green Pixel Background"
+          alt="Green Background"
           fill
-          className="object-cover"
+          className="object-contain"
           priority
         />
       </div>
 
-      {/* Overlay simbol masonic */}
+      {/* Top-left small icon */}
       <Image
-        src="/assets/asset_10.png"
-        alt="Overlay Symbol"
-        width={260}
-        height={260}
-        className="absolute top-[60px] right-[60px] z-10 opacity-10"
+        src="/assets/asset_11.png"
+        alt="Corner Logo"
+        width={48}
+        height={48}
+        className="absolute top-5 left-5 z-50"
       />
 
-      {/* Dildo stânga */}
+      {/* Overlay symbol behind logo */}
+      <Image
+        src="/assets/asset_10.png"
+        alt="Symbol Overlay"
+        width={300}
+        height={300}
+        className="absolute top-14 right-16 z-10 opacity-10"
+      />
+
+      {/* Central logo */}
+      <div className="absolute top-12 left-1/2 transform -translate-x-1/2 z-40">
+        <Image src="/assets/asset_2.png" alt="Logo" width={480} height={120} />
+      </div>
+
+      {/* Navigation buttons */}
+      <div className="absolute top-[130px] left-1/2 transform -translate-x-1/2 z-40 flex gap-4">
+        <Image src="/assets/asset_3.png" alt="Buy $DILDO" width={175} height={54} />
+        <Image src="/assets/asset_4.png" alt="Community" width={175} height={54} />
+        <Image src="/assets/asset_5.png" alt="NFT" width={175} height={54} />
+        <Image src="/assets/asset_6.png" alt="Dildo Shop" width={175} height={54} />
+      </div>
+
+      {/* Left character */}
       <Image
         src="/assets/asset_8.png"
         alt="Left Dildo"
         width={360}
-        height={620}
-        className="absolute top-[54%] left-[-20px] -translate-y-1/2 z-20"
+        height={600}
+        className="absolute bottom-0 left-[-30px] z-20"
       />
 
-      {/* Dildo dreapta */}
+      {/* Right character */}
       <Image
         src="/assets/asset_9.png"
         alt="Right Dildo"
         width={360}
-        height={620}
-        className="absolute top-[54%] right-[-20px] -translate-y-1/2 z-20"
+        height={600}
+        className="absolute bottom-0 right-[-30px] z-20"
       />
 
-      {/* Logo central */}
-      <div className="absolute top-[50px] left-1/2 transform -translate-x-1/2 z-40">
-        <Image
-          src="/assets/asset_2.png"
-          alt="Logo"
-          width={480}
-          height={120}
-        />
-      </div>
-
-      {/* Butoane */}
-      <div className="absolute top-[150px] left-1/2 transform -translate-x-1/2 z-40 flex gap-x-5">
-        <Image src="/assets/asset_3.png" alt="Buy $DILDO" width={200} height={56} />
-        <Image src="/assets/asset_4.png" alt="Community" width={200} height={56} />
-        <Image src="/assets/asset_5.png" alt="NFT" width={200} height={56} />
-        <Image src="/assets/asset_6.png" alt="Dildo Shop" width={200} height={56} />
-      </div>
-
-      {/* Imagine centrală */}
-      <div className="relative w-full flex justify-center pt-[260px] z-30">
+      {/* Central grayscale image */}
+      <div className="relative z-30 flex justify-center mt-[240px]">
         <Image
           src="/assets/asset_7.png"
-          alt="Main image"
-          width={840}
-          height={520}
-          className="z-30"
+          alt="Central Image"
+          width={860}
+          height={540}
         />
       </div>
-
-      {/* Logo mic stânga sus */}
-      <Image
-        src="/assets/asset_11.png"
-        alt="Corner Logo"
-        width={50}
-        height={50}
-        className="absolute top-4 left-4 z-50"
-      />
     </section>
   );
 }
