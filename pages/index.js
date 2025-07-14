3f00c2e8d42663c6b18abe629cd49ea9f1f7a8ec
NeoTechAi <neotechfinancecity@gmail.com>
2025-07-14 06:25:53 +0300

    sadsa

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Hero from "@/components/Hero";
-import GoGreen from "@/components/GoGreen";
-import NFTSection from "@/components/NFTSection";
-import Store from "@/components/Store";
-import Memes from "@/components/Memes";
-import Footer from "@/components/Footer";
+import Hero from "../components/Hero";
+import GoGreen from "../components/GoGreen";
+import NFTSection from "../components/NFTSection";
+import Store from "../components/Store";
+import Memes from "../components/Memes";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
