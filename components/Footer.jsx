32e2369635709f852035218ed7814146fe88776b
NeoTechAi <neotechfinancecity@gmail.com>
2025-07-14 05:42:52 +0300

    delete all

--- components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-black text-white text-center py-6">
-      <p>&copy; {new Date().getFullYear()} DILDO TOKEN. All rights reserved.</p>
-    </footer>
-  );
-}
