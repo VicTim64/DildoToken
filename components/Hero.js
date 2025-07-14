ff76ca3a5e2bb4165eae7ce129a6f6ef2139f9c9
NeoTechAi <neotechfinancecity@gmail.com>
2025-07-14 07:48:53 +0300

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -2,73 +2,75 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative w-full h-[600px] md:h-[700px] overflow-hidden">
-      {/* Asset 1: Green pixelated background */}
-      <Image
-        src="/assets/asset_1.png"
-        alt="Green Background"
-        fill
-        className="object-cover z-0"
-        priority
-      />
+    <section className="relative w-full h-[700px] md:h-[750px] overflow-hidden bg-black">
+      {/* Asset 1: Green pixelated background (centered stripe) */}
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-[55%] w-full max-w-[1400px] h-[480px] z-0">
+        <Image
+          src="/assets/asset_1.png"
+          alt="Green Background"
+          fill
+          className="object-cover"
+          priority
+        />
+      </div>
 
-      {/* Asset 10: Subtle symbol overlay behind content */}
+      {/* Asset 10: Subtle symbol overlay behind logo */}
       <Image
         src="/assets/asset_10.png"
         alt="Symbol Overlay"
-        width={250}
-        height={250}
-        className="absolute top-10 right-10 z-10 opacity-10"
+        width={260}
+        height={260}
+        className="absolute top-[60px] right-[60px] z-10 opacity-10"
       />
 
       {/* Asset 8: Left green dildo with pixel glasses */}
       <Image
         src="/assets/asset_8.png"
         alt="Left Dildo"
-        width={220}
-        height={420}
-        className="absolute bottom-4 left-2 z-20"
+        width={180}
+        height={380}
+        className="absolute top-[50%] left-0 -translate-y-1/2 z-20"
       />
 
       {/* Asset 9: Right green dildo with cigar and glasses */}
       <Image
         src="/assets/asset_9.png"
         alt="Right Dildo"
-        width={270}
-        height={470}
-        className="absolute bottom-4 right-2 z-20"
+        width={230}
+        height={420}
+        className="absolute top-[50%] right-0 -translate-y-1/2 z-20"
       />
 
-      {/* Asset 7: Central grayscale image with green arrow */}
-      <div className="relative w-full flex justify-center items-center pt-32 z-30">
+      {/* Asset 7: Central grayscale image */}
+      <div className="relative w-full flex justify-center pt-32 z-30">
         <Image
           src="/assets/asset_7.png"
           alt="Central Image"
-          width={720}
-          height={460}
+          width={800}
+          height={500}
           className="z-30"
         />
       </div>
 
-      {/* Asset 2: Logo center top */}
-      <div className="absolute top-8 left-1/2 transform -translate-x-1/2 z-40">
+      {/* Asset 2: Logo */}
+      <div className="absolute top-6 left-1/2 transform -translate-x-1/2 z-40">
         <Image
           src="/assets/asset_2.png"
           alt="Logo"
-          width={280}
+          width={300}
           height={80}
         />
       </div>
 
-      {/* Asset 3, 4, 5, 6: Buttons under logo */}
+      {/* Asset 3–6: Buttons */}
       <div className="absolute top-24 left-1/2 transform -translate-x-1/2 z-40 flex gap-3">
-        <Image src="/assets/asset_3.png" alt="Buy $DILDO" width={130} height={40} />
-        <Image src="/assets/asset_4.png" alt="Community" width={130} height={40} />
-        <Image src="/assets/asset_5.png" alt="NFT" width={130} height={40} />
-        <Image src="/assets/asset_6.png" alt="Dildo Shop" width={130} height={40} />
+        <Image src="/assets/asset_3.png" alt="Buy $DILDO" width={140} height={40} />
+        <Image src="/assets/asset_4.png" alt="Community" width={140} height={40} />
+        <Image src="/assets/asset_5.png" alt="NFT" width={140} height={40} />
+        <Image src="/assets/asset_6.png" alt="Dildo Shop" width={140} height={40} />
       </div>
 
-      {/* Asset 11: Top-left corner logo icon */}
+      {/* Asset 11: Logo top-left */}
       <Image
         src="/assets/asset_11.png"
         alt="Corner Logo"
