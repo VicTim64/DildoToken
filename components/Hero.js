a00b30073c389f7f8c9104e4de346a8b4f38bb3e
NeoTechAi <neotechfinancecity@gmail.com>
2025-07-14 13:56:18 +0300

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <section className="relative w-full min-h-screen bg-[#021a0f] overflow-hidden px-4 pb-12">
       {/* Background pixel grid */}
-      <div className="absolute top-[15vw] left-1/2 transform -translate-x-1/2 w-full max-w-[1440px] h-[35vw] z-10">
+      <div className="absolute top-[220px] left-1/2 transform -translate-x-1/2 w-full max-w-[1440px] h-[500px] z-10">
         <Image
           src="/assets/asset_1.png"
           alt="Green Background"
@@ -20,7 +20,7 @@ export default function Hero() {
         alt="Symbol Overlay"
         width={240}
         height={240}
-        className="absolute top-[8vw] right-[2.5vw] z-20 opacity-100 brightness-[2.6]"
+        className="absolute top-[120px] right-[40px] z-20 opacity-100 brightness-[2.6]"
       />
 
       {/* Corner icon */}
@@ -33,12 +33,12 @@ export default function Hero() {
       />
 
       {/* Logo */}
-      <div className="absolute top-[2vw] left-1/2 transform -translate-x-1/2 z-40">
+      <div className="absolute top-[30px] left-1/2 transform -translate-x-1/2 z-40">
         <Image src="/assets/asset_2.png" alt="Logo" width={300} height={90} />
       </div>
 
       {/* Navigation buttons */}
-      <div className="absolute top-[11vw] left-1/2 transform -translate-x-1/2 z-40 flex flex-wrap justify-center gap-4">
+      <div className="absolute top-[170px] left-1/2 transform -translate-x-1/2 z-40 flex flex-wrap justify-center gap-4">
         <Image src="/assets/asset_3.png" alt="Buy $DILDO" width={160} height={44} />
         <Image src="/assets/asset_4.png" alt="Community" width={160} height={44} />
         <Image src="/assets/asset_5.png" alt="NFT" width={160} height={44} />
@@ -46,7 +46,7 @@ export default function Hero() {
       </div>
 
       {/* Central image with frame */}
-      <div className="absolute top-[18vw] left-1/2 transform -translate-x-1/2 z-40 w-[61vw]">
+      <div className="absolute top-[280px] left-1/2 transform -translate-x-1/2 z-40">
         <div className="border-[6px] border-[#00FF00]">
           <Image
             src="/assets/asset_7.png"
@@ -58,24 +58,22 @@ export default function Hero() {
       </div>
 
       {/* Left dildo - under central image */}
-      <div className="absolute bottom-[5vw] left-1/2 transform -translate-x-[calc(50%+33vw)] z-30 w-[20vw] max-w-[288px]">
-        <Image
-          src="/assets/asset_8.png"
-          alt="Left Dildo"
-          width={288}
-          height={552}
-        />
-      </div>
+      <Image
+        src="/assets/asset_8.png"
+        alt="Left Dildo"
+        width={288}
+        height={552}
+        className="absolute bottom-[80px] left-[calc(50%-550px)] z-30"
+      />
 
       {/* Right dildo - over central image */}
-      <div className="absolute bottom-[5vw] right-1/2 transform translate-x-[calc(50%+33vw)] z-50 w-[20vw] max-w-[288px]">
-        <Image
-          src="/assets/asset_9.png"
-          alt="Right Dildo"
-          width={288}
-          height={552}
-        />
-      </div>
+      <Image
+        src="/assets/asset_9.png"
+        alt="Right Dildo"
+        width={288}
+        height={552}
+        className="absolute bottom-[80px] right-[calc(50%-550px)] z-50"
+      />
     </section>
   );
 }
