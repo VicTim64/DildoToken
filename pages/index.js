30f983439744a2b1e3120b0a13b2915da710b702
NeoTechAi <neotechfinancecity@gmail.com>
2025-07-14 03:41:57 +0300

    update

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>DILDO TOKEN</title>
+      </Head>
+      <main className="min-h-screen bg-black text-white flex items-center justify-center">
+        <h1 className="text-5xl font-bold">DILDO TOKEN IS LIVE</h1>
+      </main>
+    </>
+  )
+}
