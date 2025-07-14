dab5371cefafbcaab3a289f26bcbaa91bfe656a9
Vlad Olareanu <vlad.olareanu@gmail.com>
2025-07-14 03:44:42 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
-import Head from 'next/head'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import MemeSlider from '../components/MemeSlider'
+import Store from '../components/Store'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>DILDO TOKEN</title>
-      </Head>
-      <main className="min-h-screen bg-black text-white flex items-center justify-center">
-        <h1 className="text-5xl font-bold">DILDO TOKEN IS LIVE</h1>
-      </main>
+      <Navbar />
+      <Hero />
+      <MemeSlider />
+      <Store />
+      <Footer />
     </>
   )
 }
