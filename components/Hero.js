3ffda60ec30daae635ae2a27285a0148afb17188
NeoTechAi <neotechfinancecity@gmail.com>
2025-07-14 08:05:32 +0300

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -2,47 +2,49 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative w-full min-h-[860px] overflow-hidden bg-[#004223]">
-      {/* Asset 1: Green pixelated background */}
-      <div className="absolute top-[200px] left-0 w-full h-[540px] z-0">
+    <section className="relative w-full min-h-[860px] overflow-hidden" style={{
+      background: "linear-gradient(to bottom, #003619, #013018)"
+    }}>
+      {/* Fundal pixelat */}
+      <div className="absolute top-[210px] left-1/2 -translate-x-1/2 w-full h-[540px] z-0">
         <Image
           src="/assets/asset_1.png"
-          alt="Green Background"
+          alt="Green Pixel Background"
           fill
           className="object-cover"
           priority
         />
       </div>
 
-      {/* Asset 10: Subtle overlay */}
+      {/* Overlay simbol masonic */}
       <Image
         src="/assets/asset_10.png"
-        alt="Symbol Overlay"
+        alt="Overlay Symbol"
         width={260}
         height={260}
         className="absolute top-[60px] right-[60px] z-10 opacity-10"
       />
 
-      {/* Asset 8: Left dildo */}
+      {/* Dildo stânga */}
       <Image
         src="/assets/asset_8.png"
         alt="Left Dildo"
         width={360}
         height={620}
-        className="absolute top-[50%] left-[-20px] -translate-y-1/2 z-20"
+        className="absolute top-[54%] left-[-20px] -translate-y-1/2 z-20"
       />
 
-      {/* Asset 9: Right dildo */}
+      {/* Dildo dreapta */}
       <Image
         src="/assets/asset_9.png"
         alt="Right Dildo"
         width={360}
         height={620}
-        className="absolute top-[50%] right-[-20px] -translate-y-1/2 z-20"
+        className="absolute top-[54%] right-[-20px] -translate-y-1/2 z-20"
       />
 
-      {/* Asset 2: Main logo */}
-      <div className="absolute top-10 left-1/2 transform -translate-x-1/2 z-40">
+      {/* Logo central */}
+      <div className="absolute top-[50px] left-1/2 transform -translate-x-1/2 z-40">
         <Image
           src="/assets/asset_2.png"
           alt="Logo"
@@ -51,16 +53,16 @@ export default function Hero() {
         />
       </div>
 
-      {/* Asset 3–6: Menu buttons */}
-      <div className="absolute top-[160px] left-1/2 transform -translate-x-1/2 z-40 flex gap-x-5">
+      {/* Butoane */}
+      <div className="absolute top-[150px] left-1/2 transform -translate-x-1/2 z-40 flex gap-x-5">
         <Image src="/assets/asset_3.png" alt="Buy $DILDO" width={200} height={56} />
         <Image src="/assets/asset_4.png" alt="Community" width={200} height={56} />
         <Image src="/assets/asset_5.png" alt="NFT" width={200} height={56} />
         <Image src="/assets/asset_6.png" alt="Dildo Shop" width={200} height={56} />
       </div>
 
-      {/* Asset 7: Main central image */}
-      <div className="relative w-full flex justify-center pt-[280px] z-30">
+      {/* Imagine centrală */}
+      <div className="relative w-full flex justify-center pt-[260px] z-30">
         <Image
           src="/assets/asset_7.png"
           alt="Main image"
@@ -70,7 +72,7 @@ export default function Hero() {
         />
       </div>
 
-      {/* Asset 11: Corner logo */}
+      {/* Logo mic stânga sus */}
       <Image
         src="/assets/asset_11.png"
         alt="Corner Logo"
