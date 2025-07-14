a5cde784ea0a5c162a79a08ac6b7feac4c4db9a3
NeoTechAi <neotechfinancecity@gmail.com>
2025-07-14 08:45:20 +0300

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <section className="relative w-full min-h-screen bg-[#021a0f] overflow-hidden px-4 pb-12">
       {/* Background pixel grid */}
-      <div className="absolute top-[160px] left-1/2 transform -translate-x-1/2 w-full max-w-[1440px] h-[500px] z-10">
+      <div className="absolute top-[220px] left-1/2 transform -translate-x-1/2 w-full max-w-[1440px] h-[500px] z-10">
         <Image
           src="/assets/asset_1.png"
           alt="Green Background"
@@ -20,7 +20,7 @@ export default function Hero() {
         alt="Symbol Overlay"
         width={460}
         height={460}
-        className="absolute top-[140px] right-[40px] z-30 opacity-100 brightness-[2.2]"
+        className="absolute top-[200px] right-[40px] z-30 opacity-100 brightness-[2.2]"
       />
 
       {/* Corner icon */}
@@ -38,37 +38,37 @@ export default function Hero() {
       </div>
 
       {/* Navigation buttons */}
-      <div className="absolute top-[140px] left-1/2 transform -translate-x-1/2 z-40 flex flex-wrap justify-center gap-2">
-        <Image src="/assets/asset_3.png" alt="Buy $DILDO" width={70} height={24} />
-        <Image src="/assets/asset_4.png" alt="Community" width={70} height={24} />
-        <Image src="/assets/asset_5.png" alt="NFT" width={70} height={24} />
-        <Image src="/assets/asset_6.png" alt="Dildo Shop" width={70} height={24} />
+      <div className="absolute top-[120px] left-1/2 transform -translate-x-1/2 z-40 flex flex-wrap justify-center gap-3">
+        <Image src="/assets/asset_3.png" alt="Buy $DILDO" width={76} height={24} />
+        <Image src="/assets/asset_4.png" alt="Community" width={76} height={24} />
+        <Image src="/assets/asset_5.png" alt="NFT" width={76} height={24} />
+        <Image src="/assets/asset_6.png" alt="Dildo Shop" width={76} height={24} />
       </div>
 
       {/* Characters left & right */}
       <Image
         src="/assets/asset_8.png"
         alt="Left Dildo"
-        width={90}
-        height={180}
+        width={100}
+        height={200}
         className="absolute bottom-0 left-0 z-30"
       />
       <Image
         src="/assets/asset_9.png"
         alt="Right Dildo"
-        width={90}
-        height={180}
+        width={100}
+        height={200}
         className="absolute bottom-0 right-0 z-50"
       />
 
       {/* Center image in bordered frame */}
-      <div className="absolute top-[220px] left-1/2 transform -translate-x-1/2 z-40">
+      <div className="absolute top-[300px] left-1/2 transform -translate-x-1/2 z-40">
         <div className="border-[6px] border-[#00FF00]">
           <Image
             src="/assets/asset_7.png"
             alt="Central Image"
-            width={420}
-            height={240}
+            width={440}
+            height={260}
           />
         </div>
       </div>
