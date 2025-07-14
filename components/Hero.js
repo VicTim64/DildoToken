d491804d1edd69339cd5450a3cd16c787d2ca9ea
NeoTechAi <neotechfinancecity@gmail.com>
2025-07-14 10:46:40 +0300

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -2,118 +2,84 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative w-full min-h-screen bg-[#021a0f] overflow-hidden px-4 pb-12">
-      {/* Background pixel grid */}
-      <div className="absolute top-[220px] left-1/2 transform -translate-x-1/2 w-full max-w-[1440px] h-[500px] z-10">
+    <section className="relative w-full overflow-hidden bg-[#01110A]">
+      {/* LOGO + NAVBAR */}
+      <div className="flex flex-col items-center justify-center pt-6 md:pt-10">
         <Image
-          src="/assets/asset_1.png"
-          alt="Green Background"
-          fill
-          className="object-contain"
-          priority
-        />
-      </div>
-
-      {/* Symbol background overlay */}
-      <Image
-        src="/assets/asset_10.png"
-        alt="Symbol Overlay"
-        width={240}
-        height={240}
-        className="absolute top-[120px] right-[40px] z-20 opacity-100 brightness-[2.6] hidden md:block"
-      />
-
-      {/* Corner icon */}
-      <Image
-        src="/assets/asset_11.png"
-        alt="Corner Logo"
-        width={48}
-        height={48}
-        className="absolute top-5 left-5 z-50"
-      />
-
-      {/* Logo */}
-      <div className="absolute top-[30px] left-1/2 transform -translate-x-1/2 z-40">
-        <Image
-          src="/assets/asset_2.png"
-          alt="Logo"
+          src="/assets_hero/dildo-logo.png"
+          alt="dildo logo"
           width={300}
-          height={90}
+          height={80}
+          className="w-[200px] md:w-[300px] h-auto"
         />
-      </div>
 
-      {/* Navigation buttons */}
-      <div className="absolute top-[170px] left-1/2 transform -translate-x-1/2 z-40 flex flex-wrap justify-center gap-4 max-w-[95vw]">
-        <Image
-          src="/assets/asset_3.png"
-          alt="Buy $DILDO"
-          width={160}
-          height={44}
-        />
-        <Image
-          src="/assets/asset_4.png"
-          alt="Community"
-          width={160}
-          height={44}
-        />
-        <Image
-          src="/assets/asset_5.png"
-          alt="NFT"
-          width={160}
-          height={44}
-        />
-        <Image
-          src="/assets/asset_6.png"
-          alt="Dildo Shop"
-          width={160}
-          height={44}
-        />
-      </div>
-
-      {/* Central image with frame */}
-      <div className="absolute top-[280px] left-1/2 transform -translate-x-1/2 z-40">
-        <div className="border-[6px] border-[#00FF00] w-[90vw] max-w-[880px]">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-2 mt-4 md:mt-6 z-10">
           <Image
-            src="/assets/asset_7.png"
-            alt="Central Image"
-            width={880}
-            height={520}
-            className="w-full h-auto"
+            src="/assets_hero/buy-button.png"
+            alt="buy button"
+            width={200}
+            height={60}
+            className="w-[160px] md:w-[200px] h-auto"
+          />
+          <Image
+            src="/assets_hero/community-button.png"
+            alt="community button"
+            width={200}
+            height={60}
+            className="w-[160px] md:w-[200px] h-auto"
           />
         </div>
       </div>
 
-      {/* Left dildo - under central image */}
-      <div className="absolute bottom-[80px] left-[calc(50%-550px)] z-30 hidden md:block">
+      {/* DILDO CENTRAL */}
+      <div className="flex justify-center items-center w-full mt-6 px-4 md:px-0">
         <Image
-          src="/assets/asset_8.png"
-          alt="Left Dildo"
-          width={288}
-          height={552}
+          src="/assets_hero/dildo-central.png"
+          alt="central image"
+          width={700}
+          height={400}
+          className="max-w-full h-auto border-4 border-[#00FF00]"
         />
       </div>
 
-      {/* Right dildo - over central image */}
-      <div className="absolute bottom-[80px] right-[calc(50%-550px)] z-50 hidden md:block">
+      {/* BACKGROUND BOTTOM BAR */}
+      <div className="w-full h-16 md:h-24 bg-[url('/assets_hero/green-bar.png')] bg-repeat-x mt-4" />
+
+      {/* DILDO LEFT */}
+      <div className="absolute bottom-0 left-0 hidden md:block">
         <Image
-          src="/assets/asset_9.png"
-          alt="Right Dildo"
-          width={288}
-          height={552}
+          src="/assets_hero/dildo-left.png"
+          alt="dildo left"
+          width={160}
+          height={300}
         />
       </div>
 
-      {/* Mobile dildos */}
-      <div className="flex md:hidden justify-between absolute bottom-4 w-full px-4 z-40">
+      {/* DILDO RIGHT */}
+      <div className="absolute bottom-0 right-0 hidden md:block">
         <Image
-          src="/assets/asset_8.png"
-          alt="Left Dildo"
+          src="/assets_hero/dildo-right.png"
+          alt="dildo right"
+          width={160}
+          height={300}
+        />
+      </div>
+
+      {/* DILDO LEFT MOBILE */}
+      <div className="absolute bottom-0 left-0 md:hidden">
+        <Image
+          src="/assets_hero/dildo-left.png"
+          alt="dildo left mobile"
           width={80}
           height={160}
         />
+      </div>
+
+      {/* DILDO RIGHT MOBILE */}
+      <div className="absolute bottom-0 right-0 md:hidden">
         <Image
-          src="/assets/asset_9.png"
-          alt="Right Dildo"
+          src="/assets_hero/dildo-right.png"
+          alt="dildo right mobile"
           width={80}
           height={160}
         />
