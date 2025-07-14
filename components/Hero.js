63678cf7004bc36fb66546c87f7bfb54deaa7edc
NeoTechAi <neotechfinancecity@gmail.com>
2025-07-14 14:08:34 +0300

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative w-full min-h-screen bg-[#021a0f] overflow-hidden px-4 pb-12">
+    <section className="relative w-full min-h-screen bg-[#021a0f] overflow-hidden px-[5vw] pb-[10vh]">
       {/* Background pixel grid */}
-      <div className="absolute top-[220px] left-1/2 transform -translate-x-1/2 w-full max-w-[1440px] h-[500px] z-10">
+      <div className="absolute top-[25vh] left-1/2 transform -translate-x-1/2 w-full max-w-[90vw] h-[35vh] z-10">
         <Image
           src="/assets/asset_1.png"
           alt="Green Background"
@@ -15,65 +15,104 @@ export default function Hero() {
       </div>
 
       {/* Symbol background overlay */}
-      <Image
-        src="/assets/asset_10.png"
-        alt="Symbol Overlay"
-        width={240}
-        height={240}
-        className="absolute top-[120px] right-[40px] z-20 opacity-100 brightness-[2.6]"
-      />
+      <div className="absolute top-[10vh] right-[3vw] z-20 w-[12vw] max-w-[120px]">
+        <Image
+          src="/assets/asset_10.png"
+          alt="Symbol Overlay"
+          width={240}
+          height={240}
+          className="w-full h-auto opacity-100 brightness-[2.6]"
+        />
+      </div>
 
       {/* Corner icon */}
-      <Image
-        src="/assets/asset_11.png"
-        alt="Corner Logo"
-        width={48}
-        height={48}
-        className="absolute top-5 left-5 z-50"
-      />
+      <div className="absolute top-[2vh] left-[2vw] z-50 w-[4vw] max-w-[48px]">
+        <Image
+          src="/assets/asset_11.png"
+          alt="Corner Logo"
+          width={48}
+          height={48}
+          className="w-full h-auto"
+        />
+      </div>
 
       {/* Logo */}
-      <div className="absolute top-[30px] left-1/2 transform -translate-x-1/2 z-40">
-        <Image src="/assets/asset_2.png" alt="Logo" width={300} height={90} />
+      <div className="absolute top-[5vh] left-1/2 transform -translate-x-1/2 z-40 w-[30vw] max-w-[300px]">
+        <Image
+          src="/assets/asset_2.png"
+          alt="Logo"
+          width={300}
+          height={90}
+          className="w-full h-auto"
+        />
       </div>
 
       {/* Navigation buttons */}
-      <div className="absolute top-[170px] left-1/2 transform -translate-x-1/2 z-40 flex flex-wrap justify-center gap-4">
-        <Image src="/assets/asset_3.png" alt="Buy $DILDO" width={160} height={44} />
-        <Image src="/assets/asset_4.png" alt="Community" width={160} height={44} />
-        <Image src="/assets/asset_5.png" alt="NFT" width={160} height={44} />
-        <Image src="/assets/asset_6.png" alt="Dildo Shop" width={160} height={44} />
+      <div className="absolute top-[18vh] left-1/2 transform -translate-x-1/2 z-40 flex flex-wrap justify-center gap-[1vw] w-full max-w-[90vw]">
+        <Image
+          src="/assets/asset_3.png"
+          alt="Buy $DILDO"
+          width={160}
+          height={44}
+          className="w-[20vw] max-w-[160px] h-auto"
+        />
+        <Image
+          src="/assets/asset_4.png"
+          alt="Community"
+          width={160}
+          height={44}
+          className="w-[20vw] max-w-[160px] h-auto"
+        />
+        <Image
+          src="/assets/asset_5.png"
+          alt="NFT"
+          width={160}
+          height={44}
+          className="w-[20vw] max-w-[160px] h-auto"
+        />
+        <Image
+          src="/assets/asset_6.png"
+          alt="Dildo Shop"
+          width={160}
+          height={44}
+          className="w-[20vw] max-w-[160px] h-auto"
+        />
       </div>
 
       {/* Central image with frame */}
-      <div className="absolute top-[280px] left-1/2 transform -translate-x-1/2 z-40">
-        <div className="border-[6px] border-[#00FF00]">
+      <div className="absolute top-[28vh] left-1/2 transform -translate-x-1/2 z-40 w-[60vw] max-w-[880px]">
+        <div className="border-[0.6vw] border-[#00FF00]">
           <Image
             src="/assets/asset_7.png"
             alt="Central Image"
             width={880}
             height={520}
+            className="w-full h-auto"
           />
         </div>
       </div>
 
-      {/* Left dildo - under central image */}
-      <Image
-        src="/assets/asset_8.png"
-        alt="Left Dildo"
-        width={288}
-        height={552}
-        className="absolute bottom-[80px] left-[calc(50%-550px)] z-30"
-      />
+      {/* Left dildo */}
+      <div className="absolute bottom-[10vh] left-[calc(50%-38vw)] z-30 w-[20vw] max-w-[288px]">
+        <Image
+          src="/assets/asset_8.png"
+          alt="Left Dildo"
+          width={288}
+          height={552}
+          className="w-full h-auto"
+        />
+      </div>
 
-      {/* Right dildo - over central image */}
-      <Image
-        src="/assets/asset_9.png"
-        alt="Right Dildo"
-        width={288}
-        height={552}
-        className="absolute bottom-[80px] right-[calc(50%-550px)] z-50"
-      />
+      {/* Right dildo */}
+      <div className="absolute bottom-[10vh] right-[calc(50%-38vw)] z-50 w-[20vw] max-w-[288px]">
+        <Image
+          src="/assets/asset_9.png"
+          alt="Right Dildo"
+          width={288}
+          height={552}
+          className="w-full h-auto"
+        />
+      </div>
     </section>
   );
 }
