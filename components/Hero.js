626dcb61e6d7addd9997d24e5fcf31822bf9e44b
NeoTechAi <neotechfinancecity@gmail.com>
2025-07-14 07:46:29 +0300

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative w-full h-screen overflow-hidden">
+    <section className="relative w-full h-[600px] md:h-[700px] overflow-hidden">
       {/* Asset 1: Green pixelated background */}
       <Image
         src="/assets/asset_1.png"
@@ -16,64 +16,64 @@ export default function Hero() {
       <Image
         src="/assets/asset_10.png"
         alt="Symbol Overlay"
-        width={200}
-        height={200}
-        className="absolute top-0 right-0 z-10 opacity-10"
+        width={250}
+        height={250}
+        className="absolute top-10 right-10 z-10 opacity-10"
       />
 
       {/* Asset 8: Left green dildo with pixel glasses */}
       <Image
         src="/assets/asset_8.png"
         alt="Left Dildo"
-        width={200}
-        height={400}
-        className="absolute bottom-0 left-0 z-20"
+        width={220}
+        height={420}
+        className="absolute bottom-4 left-2 z-20"
       />
 
       {/* Asset 9: Right green dildo with cigar and glasses */}
       <Image
         src="/assets/asset_9.png"
         alt="Right Dildo"
-        width={250}
-        height={450}
-        className="absolute bottom-0 right-0 z-20"
+        width={270}
+        height={470}
+        className="absolute bottom-4 right-2 z-20"
       />
 
       {/* Asset 7: Central grayscale image with green arrow */}
-      <div className="relative w-full flex justify-center items-center pt-24 z-30">
+      <div className="relative w-full flex justify-center items-center pt-32 z-30">
         <Image
           src="/assets/asset_7.png"
           alt="Central Image"
-          width={600}
-          height={400}
+          width={720}
+          height={460}
           className="z-30"
         />
       </div>
 
       {/* Asset 2: Logo center top */}
-      <div className="absolute top-6 left-1/2 transform -translate-x-1/2 z-40">
+      <div className="absolute top-8 left-1/2 transform -translate-x-1/2 z-40">
         <Image
           src="/assets/asset_2.png"
           alt="Logo"
-          width={200}
-          height={60}
+          width={280}
+          height={80}
         />
       </div>
 
       {/* Asset 3, 4, 5, 6: Buttons under logo */}
-      <div className="absolute top-20 left-1/2 transform -translate-x-1/2 z-40 flex gap-2">
-        <Image src="/assets/asset_3.png" alt="Buy $DILDO" width={110} height={30} />
-        <Image src="/assets/asset_4.png" alt="Community" width={110} height={30} />
-        <Image src="/assets/asset_5.png" alt="NFT" width={110} height={30} />
-        <Image src="/assets/asset_6.png" alt="Dildo Shop" width={110} height={30} />
+      <div className="absolute top-24 left-1/2 transform -translate-x-1/2 z-40 flex gap-3">
+        <Image src="/assets/asset_3.png" alt="Buy $DILDO" width={130} height={40} />
+        <Image src="/assets/asset_4.png" alt="Community" width={130} height={40} />
+        <Image src="/assets/asset_5.png" alt="NFT" width={130} height={40} />
+        <Image src="/assets/asset_6.png" alt="Dildo Shop" width={130} height={40} />
       </div>
 
       {/* Asset 11: Top-left corner logo icon */}
       <Image
         src="/assets/asset_11.png"
         alt="Corner Logo"
-        width={40}
-        height={40}
+        width={50}
+        height={50}
         className="absolute top-4 left-4 z-50"
       />
     </section>
